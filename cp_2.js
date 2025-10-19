2c1964e7bb1696a272bdb8cdab0fd206213343ed
Kameron Thibault <kameronthibault@usf.edu>
2025-10-19 11:47:09 -0400

    handle error and fetchproductsasync created

--- cp_2.js
+++ cp_2.js
@@ -15,3 +15,15 @@ return response.json();
     console.error (`Fetch error:`, error);
 });
 }
+async function fetchProductsAsync(){
+    try {
+        const response = await fetch(`https://www.course-api.com/javascript-store-products`)
+    if (!response.ok) {
+        throw new Error (`http Error, Status: ${response.status}`);
+    }
+    const products = await response.json();
+    displayProducts(products);
+    }catch (error){
+        handleError(error);
+    }
+}
